47777232e100caf486f4e07a12210abe104462dc
seonghye0n <seonghye0n@kakao.com>
2023-03-30 13:53:18 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,21 +1,16 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int max = 0, index = 0;
+        List<Integer> list = new ArrayList<>();
 
         for (int i = 1; i <= 9; i++) {
-            int num = sc.nextInt();
-
-            if (max < num) {
-                max = num;
-                index = i;
-            }
+            list.add(sc.nextInt());
         }
 
+        int max = Collections.max(list);
         System.out.println(max);
-        System.out.println(index);
+        System.out.println(list.indexOf(max) + 1);
     }
 }
