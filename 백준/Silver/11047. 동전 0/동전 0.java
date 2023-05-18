cb37ad4c7240c9161ee3047050c9a032dbb774ab
seonghye0n <seonghye0n@kakao.com>
2023-05-18 19:49:02 +0900

    [Silver IV] Title: 동전 0, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int coinCnt = 0;
+		for (int i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] > k)
+				continue;
+
+			coinCnt += k / arr[i];
+			k = k % arr[i];
+		}
+
+		System.out.println(coinCnt);
+	}
+}
