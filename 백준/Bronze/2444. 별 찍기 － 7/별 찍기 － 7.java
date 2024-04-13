ecb1e24c57e3871bc7cc0489366289a547b1d835
SeongHyeon Park <seonghye0n@kakao.com>
2024-04-13 21:10:24 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 588 ms, Memory: 22188 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = Integer.parseInt(sc.nextLine());
+
+		String blank = " ";
+		String star = "*";
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 0; j < N - i; j++)
+				System.out.print(blank);
+			for (int j = 0; j < i * 2 - 1; j++)
+				System.out.print(star);
+			System.out.println();
+		}
+
+		for (int i = N - 1; i >= 0; i--) {
+			for (int j = 0; j < N - i; j++)
+				System.out.print(blank);
+			for (int j = 0; j < i * 2 - 1; j++)
+				System.out.print(star);
+			System.out.println();
+		}
+	}
+}
